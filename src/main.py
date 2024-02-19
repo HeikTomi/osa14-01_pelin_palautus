d68d2d09758747255c589b96b80904eacd68d774
HeikTomi <heikkinentomi@hotmail.com>
2024-02-19 20:11:32 +0200

    coin added

--- src/main.py
+++ src/main.py
@@ -11,12 +11,28 @@ class Npc:
         self.rect.topleft = [x, y]
         self.speed = 1
         
+class Coin(Npc):
+    def __init__(self, x: int, y: int, image: str):
+        super().__init__(x, y, image)
+        
+    def move_coin(self, maxwidth, maxheight):
+        if self.y <= 250:
+                self.x = random.randrange(maxwidth-50)
+                self.y = maxheight-self.image.get_height()-25
+                self.rect.topleft = [self.x,self.y]
+        else: 
+            self.x = random.randrange(maxwidth-50)
+            self.y = random.randrange(25)
+            self.rect.topleft = [self.x, self.y]
+       
 class Player(Npc):
     def __init__(self, x: int, y: int, image: str):
         super().__init__(x, y, image)
         self.speed = 1.5
         self.lives = 3
+        self.level = 1
         self.score = 0
+        self.counter = 0
         self.to_right = False
         self.to_left = False
         self.to_up = False
@@ -36,17 +52,31 @@ class Player(Npc):
             if not self.y >= sheight-self.image.get_height():
                 self.y += self.speed
         
-        self.rect.topleft = [self.x, self.y]         
+        self.rect.topleft = [self.x, self.y]
+        
+    def check_coin_collision(self, coin, maxwidth, maxheight):
+        if self.check_collision(coin.rect):
+            self.score += 100
+            self.counter += 100
+            if self.counter >= 1000:
+                self.level += 1
+                self.counter = 0
+                
+            coin.move_coin(maxwidth, maxheight)
+                
+            print(self.score)
+        
+    def check_collision(self, target):
+        return pygame.Rect.colliderect(self.rect, target)    
         
 class Game:
     def __init__(self):
-        self.level = 1
         self.swidth = 800
         self.sheight = 600
         self.fps = 120
         self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((self.swidth, self.sheight))
-        self.color_black = (0,0,0)
+        self.bg_color = (0,0,0)
         
     def input_events(self, player):  
         for event in pygame.event.get():
@@ -79,11 +109,12 @@ class Game:
         pygame.display.set_caption('Coin collector')
         
         player = Player(self.swidth//2-25, self.sheight-100, "robo.png")
+        coin = Coin(random.randrange(self.swidth+50), random.randrange(25), "kolikko.png")
         
         while True:
             self.input_events(player)
             
-            self.screen.fill(self.color_black)
+            self.screen.fill(self.bg_color)
             
             # Draw player
             player.move_player(self.swidth, self.sheight)
@@ -91,9 +122,11 @@ class Game:
             
             # TODO: NPC creation
             
-            # TODO: Coin creation
+            # Draw coin
+            self.screen.blit(coin.image, coin.rect)
             
             # TODO: Collision detection
+            player.check_coin_collision(coin, self.swidth, self.sheight)
             
             # TODO: Info bar
             
